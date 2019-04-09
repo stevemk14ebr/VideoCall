50a1c4bef1494beba22547d1a95f69519b35ce58
Steve Eckels <stevemk14ebr@gmail.com>
2019-04-09 12:42:37 -0400

    Added server

---
+++ server/signal.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+app.get('/', function(req, res){
+  res.write('This is the relay server');
+  res.end();
+});
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', (socket) => 
+{ 
+	console.log('someone connected!');
+});
+
+const signalServer = require('simple-signal-server')(io)
+const allIDs = new Set()
+
+signalServer.on('discover', (request) => {
+  console.log('discovering');
+  const clientID = request.socket.id // you can use any kind of identity, here we use socket.id
+  allIDs.add(clientID) // keep track of all connected peers
+  request.discover(clientID, Array.from(allIDs)) // respond with id and list of other peers
+})
+
+signalServer.on('disconnect', (socket) => {
+  console.log('disconnecting');
+  const clientID = socket.id
+  allIDs.delete(clientID)
+})
+
+signalServer.on('request', (request) => {
+  request.forward() // forward all requests to connect
+})
+
+server.listen(3001, function(){
+  console.log('listening on *:3001');
+});
